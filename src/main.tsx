326f25b7b3a23a18ad5a6d219a1ee620b927364b
Your Name <you@example.com>
2023-09-05 12:03:19 +0100

    applied chakra

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
+    <ChakraProvider theme={theme}>
         <App />
-    </React.StrictMode>
+    </ChakraProvider>
 );
